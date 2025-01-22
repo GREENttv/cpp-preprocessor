d147d61da1d49fb950ce9fc7d0d6d4fc6bed18ed
Aiub Tataev <aiubtataev@gmail.com>
2025-01-22 23:02:27 +0100

    preprocessor_aiub1

--- main.cpp
+++ main.cpp
@@ -1,96 +1,44 @@
-#include <cassert>
-#include <filesystem>
-#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
 #include <iostream>
-#include <regex>
-#include <sstream>
-#include <string>
+#include <random>
 #include <vector>
 
 using namespace std;
-using filesystem::path;
 
-path operator""_p(const char* data, std::size_t sz) {
-    return path(data, data + sz);
-}
+int EffectiveCount(const vector<int>& v, int n, int i) {
+    int64_t expected_count = static_cast<int64_t>(v.size()) * (i + 1) / (n + 1);
+    double threshold = log2(v.size());
 
-// напишите эту функцию
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
-
-string GetFileContents(string file) {
-    ifstream stream(file);
-
-    // конструируем string по двум итераторам
-    return {(istreambuf_iterator<char>(stream)), istreambuf_iterator<char>()};
-}
-
-void Test() {
-    error_code err;
-    filesystem::remove_all("sources"_p, err);
-    filesystem::create_directories("sources"_p / "include2"_p / "lib"_p, err);
-    filesystem::create_directories("sources"_p / "include1"_p, err);
-    filesystem::create_directories("sources"_p / "dir1"_p / "subdir"_p, err);
-
-    {
-        ofstream file("sources/a.cpp");
-        file << "// this comment before include\n"
-                "#include \"dir1/b.h\"\n"
-                "// text between b.h and c.h\n"
-                "#include \"dir1/d.h\"\n"
-                "\n"
-                "int SayHello() {\n"
-                "    cout << \"hello, world!\" << endl;\n"
-                "#   include<dummy.txt>\n"
-                "}\n"s;
+    if (expected_count <= threshold) {
+        cout << "Using find_if" << endl;
+        auto it = find_if(v.begin(), v.end(), [i](int x) { return x > i; });
+        return distance(v.begin(), it);
+    } else {
+        cout << "Using upper_bound" << endl;
+        auto it = upper_bound(v.begin(), v.end(), i);
+        return distance(v.begin(), it);
     }
-    {
-        ofstream file("sources/dir1/b.h");
-        file << "// text from b.h before include\n"
-                "#include \"subdir/c.h\"\n"
-                "// text from b.h after include"s;
-    }
-    {
-        ofstream file("sources/dir1/subdir/c.h");
-        file << "// text from c.h before include\n"
-                "#include <std1.h>\n"
-                "// text from c.h after include\n"s;
-    }
-    {
-        ofstream file("sources/dir1/d.h");
-        file << "// text from d.h before include\n"
-                "#include \"lib/std2.h\"\n"
-                "// text from d.h after include\n"s;
-    }
-    {
-        ofstream file("sources/include1/std1.h");
-        file << "// std1\n"s;
-    }
-    {
-        ofstream file("sources/include2/lib/std2.h");
-        file << "// std2\n"s;
-    }
-
-    assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
-                                  {"sources"_p / "include1"_p,"sources"_p / "include2"_p})));
-
-    ostringstream test_out;
-    test_out << "// this comment before include\n"
-                "// text from b.h before include\n"
-                "// text from c.h before include\n"
-                "// std1\n"
-                "// text from c.h after include\n"
-                "// text from b.h after include\n"
-                "// text between b.h and c.h\n"
-                "// text from d.h before include\n"
-                "// std2\n"
-                "// text from d.h after include\n"
-                "\n"
-                "int SayHello() {\n"
-                "    cout << \"hello, world!\" << endl;\n"s;
-
-    assert(GetFileContents("sources/a.in"s) == test_out.str());
 }
 
 int main() {
-    Test();
+    static const int NUMBERS = 1'000'000;
+    static const int MAX = 1'000'000'000;
+
+    mt19937 r;
+    uniform_int_distribution<int> uniform_dist(0, MAX);
+
+    vector<int> nums;
+    for (int i = 0; i < NUMBERS; ++i) {
+        int random_number = uniform_dist(r);
+        nums.push_back(random_number);
+    }
+    sort(nums.begin(), nums.end());
+
+    int i;
+    cin >> i;
+    int result = EffectiveCount(nums, MAX, i);
+    cout << "Total numbers before "s << i << ": "s << result << endl;
+    return 0;
 }
